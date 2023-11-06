d12ead0a1f6f7b6b6e588c47054135d70b0a2213
Polina <polina@blsv.space>
2023-11-06 11:32:18 +0100

    init

---
+++ app/flotr2/page.js
@@ -0,0 +1,10 @@
+
+const Flotr2 = () => {
+    return (
+        <div>
+            Flort 2
+        </div>
+    )
+}
+
+export default Flotr2
---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+const Home = () => {
+  return (
+    <div className="grid">
+
+      Home
+
+    </div>
+  )
+}
+
+export default Home
