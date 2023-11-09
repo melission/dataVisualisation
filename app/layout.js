a0459877a8c62fef0ad0f9d2800ad9b1ff8496a7
Polina <polina@blsv.space>
2023-11-09 14:09:05 +0100

    navigation block was added.

--- app/layout.js
+++ app/layout.js
@@ -12,11 +12,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
       <body className={inter.className}>
         <nav>
-          <div className="flex flex-col md:flex-row bg-blue-700 justify-center items-center h-12 min-w-full">
-            <Link className='w-1/4' href='/'>Main</Link>
-            <Link href='/flotr2'>Flotr2</Link>
+          <div className="flex flex-col md:flex-row bg-blue-700 justify-evenly items-center h-12 min-w-full">
+            <div className='hover:scale-105'>
+              <Link href='/'>Main</Link>
+            </div>
+            <div className='hover:scale-105'>
+              <Link href='/flotr2'>Flotr2</Link>
+            </div>
           </div>
         </nav>
         <main>{children}</main>
